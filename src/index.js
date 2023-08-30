126de945e86bf0e05b661855f372687113086cc8
Br4z <bcalderonprieto@gmail.com>
2023-08-30 16:00:58 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import Experience from './Experience'
+import { Canvas } from '@react-three/fiber'
+import { OrbiControls } from '@react-three/drei'
+import './styles.css'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+	<Canvas
+		camera={ { position: [2, 0, 5] } }
+	>
+		<Experience />
+	</Canvas>
+)
