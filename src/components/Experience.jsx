f8183088dbc2665a958a861b98a9dc9c90095582
Br4z <67658575+Br4z@users.noreply.github.com>
2023-09-18 15:08:09 -0500

    Add section 2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,42 +1,21 @@
-import { OrbitControls } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
-import { useRef } from 'react'
-
+import { OrbitControls } from '@react-three/drei'
+import Bee from '../world/Bee'
+import Tree from '../world/Tree'
+import WoodenFence from '../world/WoodenFence'
 
 const Experience = () => {
-	const boxRef = useRef()
-	const sphereRef= useRef()
-	const torusRef = useRef()
-
-
-	useFrame((state, delta) => {
-		boxRef.current.rotation.x += 1 * delta;
-		torusRef.current.position.y = Math.sin(state.clock.getElapsedTime());
-		torusRef.current.rotation.y = Math.cos(state.clock.getElapsedTime());
-		sphereRef.current.position.x = Math.sin(state.clock.getElapsedTime()) + 4;
-	})
-
 	return (
 		<>
-			<OrbitControls makeDefault />
-			<ambientLight intensity={ 0.5 } />
+			<ambientLight intensity={0.5} />
 			<directionalLight position={[10, 10, 5]} intensity={2} />
-
-			<mesh ref={boxRef} position={[-4, 0, 0]}>
-				<boxGeometry args={[2, 2, 2]} />
-				<meshStandardMaterial color='yellow' />
+			<OrbitControls makeDefault />
+			<Bee position-x={2} scale={1.5} />
+			<Tree position={[-2, -2, 2]} scale={0.04} />
+			<WoodenFence position={[0, -2, 0]} />
+			<mesh position-y={-2} rotation-x={-Math.PI / 2} >
+				<planeGeometry attach='geometry' args={[12, 12]} />
+				<meshStandardMaterial attach='material' color='green' />
 			</mesh>
-
-			<mesh ref={torusRef} position={[0, 0, 0]} rotation={[-Math.PI / 4, 0, 0]}>
-				<torusGeometry args={[1, 0.4, 12, 48]} />
-				<meshStandardMaterial wireframe color="purple" />
-			</mesh>
-
-			<mesh ref={sphereRef} position={[5, 0, 0]}>
-				<sphereGeometry args={[1, 32, 16]} />
-				<meshToonMaterial color="red" />
-			</mesh>
-
 		</>
 	)
 }
